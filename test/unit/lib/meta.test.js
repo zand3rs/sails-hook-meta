f95c54e10c8c0f4f07dda654b640b530491ee814
zander <aamagtipon@voyagerinnovation.com>
2015-12-21 17:50:54 +0800

    refactored #set() method to support array parameters

--- test/unit/lib/meta.test.js
+++ test/unit/lib/meta.test.js
@@ -14,43 +14,50 @@ describe(TEST_NAME, function() {
   });
 
   describe("#set()", function() {
-    it("should return false", function() {
+    it("should return false given an invalid argument", function() {
       expect(meta).itself.to.respondTo("set");
       expect(meta.set()).to.be.false;
       expect(meta.set({})).to.be.false;
       expect(meta.set(new Object())).to.be.false;
       expect(meta.set(new Function())).to.be.false;
-      expect(meta.set({ key1: "val1", key2: "val2" })).to.be.false;
     });
 
-    it("should return true", function() {
-      expect(meta.set({ key: "val" })).to.be.true;
-      expect(meta._meta).to.have.property("key-val")
-                        .that.eql([{ key: "val" }]);
+    describe("with only one argument given", function() {
+      it("should only accept single key-value pair object parameter", function() {
+        expect(meta.set({ key1: "val1", key2: "val2" })).to.be.false;
 
-      expect(meta.set({ key: "val1" })).to.be.true;
-      expect(meta._meta).to.have.property("key-val1")
-                        .that.eql([{ key: "val1" }]);
-
-      expect(meta.set({ key1: "val1" }, { key2: "val2" }, { key1: "val3" })).to.be.true;
-      expect(meta._meta).to.have.property("key1-val1")
-                        .that.eql([{ key1: "val1", key2: "val2" }]);
-
-      expect(meta.set({ key1: "val1" }, { key2: "val2", key3: "val3" })).to.be.true;
-      expect(meta._meta).to.have.property("key1-val1")
-                        .that.eql([{ key1: "val1", key2: "val2", key3: "val3" }]);
+        expect(meta.set({ key: "val" })).to.be.true;
+        expect(meta._meta).to.have.property("key-val")
+                          .that.eql([{ key: "val" }]);
+      });
     });
 
-    it("should support attribute keys with dash", function() {
-      expect(meta.set({ "key-1-2-3": "val-4-5-6" })).to.be.true;
-      expect(meta._meta).to.have.property("key-1-2-3-val-4-5-6")
-                        .that.eql([{ "key-1-2-3": "val-4-5-6" }]);
+    describe("with two arguments given", function() {
+      it("should accept object as second parameter", function() {
+        expect(meta.set({ key1: "val1" }, { key2: "val2", key3: "val3" })).to.be.true;
+        expect(meta._meta).to.have.property("key1-val1")
+                          .that.eql([{ key1: "val1", key2: "val2", key3: "val3" }]);
+      });
+
+      it("should accept array as second parameter", function() {
+        expect(meta.set({ key1: "val1" }, [{ key2: "val2" }, { key3: "val3" }])).to.be.true;
+        expect(meta._meta).to.have.property("key1-val1")
+                          .that.eql([{ key1: "val1", key2: "val2" }, { key1: "val1", key3: "val3" }]);
+      });
     });
 
-    it("should remove invalid characters from the key", function() {
-      expect(meta.set({ "key-1-2-3": "val=4;5.6" })).to.be.true;
-      expect(meta._meta).to.have.property("key-1-2-3-val456")
-                        .that.eql([{ "key-1-2-3": "val=4;5.6" }]);
+    describe("with special characters", function() {
+      it("should support attribute keys with dash", function() {
+        expect(meta.set({ "key-1-2-3": "val-4-5-6" })).to.be.true;
+        expect(meta._meta).to.have.property("key-1-2-3-val-4-5-6")
+                          .that.eql([{ "key-1-2-3": "val-4-5-6" }]);
+      });
+
+      it("should remove invalid characters from the key", function() {
+        expect(meta.set({ "key-1-2-3": "val=4;5.6" })).to.be.true;
+        expect(meta._meta).to.have.property("key-1-2-3-val456")
+                          .that.eql([{ "key-1-2-3": "val=4;5.6" }]);
+      });
     });
   });
 
